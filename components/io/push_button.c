8782abde2c3d77afd2423c4197abe207b02c745d
sayedMurtadha <s.murtadha98@gmail.com>
2022-11-03 00:40:04 +0300

    push button fixes

--- components/io/push_button.c
+++ components/io/push_button.c
@@ -13,10 +13,11 @@
 #define DEBOUNCE_PRESET_MS 250
 
 static QueueHandle_t gpio_evt_queue = NULL;
+static uint32_t gpio_to_send = CONFIG_GPIO_INPUT_PUSH_BUTTON;
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
-    uint32_t gpio_num = (uint32_t) arg;
+    uint32_t gpio_num = *((uint32_t*)arg);
     if(gpio_num == CONFIG_GPIO_INPUT_PUSH_BUTTON){
         xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
     }
@@ -30,7 +31,7 @@ static void button_pushed(void* arg)
 
             /* Debouncing Wait */
             vTaskDelay(DEBOUNCE_PRESET_MS / portTICK_PERIOD_MS);
-            printf("Button is pushed");
+            printf("Button is pushed\n");
             xQueueReset(gpio_evt_queue);
             xTaskNotifyGive(get_rtc_task_handle());
         }
@@ -42,17 +43,15 @@ void push_button_init(void){
     //zero-initialize the config structure.
     gpio_config_t io_conf = {};
     //disable pull-down mode
-    io_conf.pull_down_en = 0;
+    io_conf.pull_down_en = 1;
     //disable pull-up mode
-    io_conf.pull_up_en = 1;
+    io_conf.pull_up_en = 0;
     //interrupt of rising edge
     io_conf.intr_type = GPIO_INTR_POSEDGE;
     //bit mask of the pins, use GPIO4/5 here
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     //set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
-    io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
 
     //create a queue to handle gpio event from isr
@@ -63,6 +62,6 @@ void push_button_init(void){
     //install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(CONFIG_GPIO_INPUT_PUSH_BUTTON, gpio_isr_handler, (void*) CONFIG_GPIO_INPUT_PUSH_BUTTON);
+    gpio_isr_handler_add(CONFIG_GPIO_INPUT_PUSH_BUTTON, gpio_isr_handler, (void*) &gpio_to_send);
 }
 
