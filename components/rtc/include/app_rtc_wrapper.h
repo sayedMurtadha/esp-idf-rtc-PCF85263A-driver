bd9b8bce278d9458511c0ede15d0df973bf0e699
sayedMurtadha <s.murtadha98@gmail.com>
2022-11-03 00:49:26 +0300

    rtc wrapper write code fix

--- components/rtc/include/app_rtc_wrapper.h
+++ components/rtc/include/app_rtc_wrapper.h
@@ -3,13 +3,13 @@
 
 #include "pcf85263.h"
 
-void rtc_time_add_seconds(pcf85263_datetime_t* datetime, uint8_t seconds);
-void rtc_time_add_minutes(pcf85263_datetime_t* datetime, uint8_t minutes);
-void rtc_time_add_hours(pcf85263_datetime_t* datetime, uint8_t hours);
+void rtc_time_add_seconds(pcf85263_datetime_t* datetime, int seconds);
+void rtc_time_add_minutes(pcf85263_datetime_t* datetime, int minutes);
+void rtc_time_add_hours(pcf85263_datetime_t* datetime, int hours);
 
-void rtc_date_add_days(pcf85263_datetime_t* datetime, uint8_t days);
-void rtc_date_add_months(pcf85263_datetime_t* datetime, uint8_t months);
-void rtc_date_add_years(pcf85263_datetime_t* datetime, uint8_t years);
+void rtc_date_add_days(pcf85263_datetime_t* datetime, int days);
+void rtc_date_add_months(pcf85263_datetime_t* datetime, int months);
+void rtc_date_add_years(pcf85263_datetime_t* datetime, int years);
 
 void rtc_register_read(uint8_t reg_addr, uint8_t *data, size_t len);
 void rtc_register_write(uint8_t reg_addr, const uint8_t *data, size_t len);
--- components/rtc/rtc_wrapper.c
+++ components/rtc/rtc_wrapper.c
@@ -23,30 +23,30 @@ do { \
 } while(0)
 
 
-void rtc_time_add_seconds(pcf85263_datetime_t* datetime, uint8_t seconds){
+void rtc_time_add_seconds(pcf85263_datetime_t* datetime, int seconds){
     ADD_MOD(datetime->time.second, seconds, 60);
 }
 
-void rtc_time_add_minutes(pcf85263_datetime_t* datetime, uint8_t minutes){
+void rtc_time_add_minutes(pcf85263_datetime_t* datetime, int minutes){
     ADD_MOD(datetime->time.minute, minutes, 60);
 }
 
-void rtc_time_add_hours(pcf85263_datetime_t* datetime, uint8_t hours){
+void rtc_time_add_hours(pcf85263_datetime_t* datetime, int hours){
     ADD_MOD(datetime->time.hour, hours, 24);
 }
 
 
-void rtc_date_add_days(pcf85263_datetime_t* datetime, uint8_t days){
+void rtc_date_add_days(pcf85263_datetime_t* datetime, int days){
     ADD_MOD_START_ONE(datetime->date.day, days, 32);
 
 }
 
-void rtc_date_add_months(pcf85263_datetime_t* datetime, uint8_t months){
+void rtc_date_add_months(pcf85263_datetime_t* datetime, int months){
     ADD_MOD_START_ONE(datetime->date.month, months, 13);
 
 }
 
-void rtc_date_add_years(pcf85263_datetime_t* datetime, uint8_t years){
+void rtc_date_add_years(pcf85263_datetime_t* datetime, int years){
     ADD_MOD_START_ONE(datetime->date.year, years, 32);
 
 }
@@ -56,15 +56,53 @@ void rtc_date_add_years(pcf85263_datetime_t* datetime, uint8_t years){
  * @brief Read a sequence of bytes from the registers
  */
 void rtc_register_read(uint8_t reg_addr, uint8_t *data, size_t len){
-    i2c_master_write_to_device(I2C_MASTER_NUM, get_pcf85263_device_address(), &reg_addr, 1, I2C_MASTER_TIMEOUT_TICKS);
-    i2c_master_read_from_device(I2C_MASTER_NUM, get_pcf85263_device_address(), data, len, I2C_MASTER_TIMEOUT_TICKS);
+    i2c_master_write_read_device(I2C_MASTER_NUM, get_pcf85263_device_address(), &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_TICKS);
+    // i2c_master_write_to_device(I2C_MASTER_NUM, get_pcf85263_device_address(), &reg_addr, 1, I2C_MASTER_TIMEOUT_TICKS);
+    // i2c_master_read_from_device(I2C_MASTER_NUM, get_pcf85263_device_address(), data, len, I2C_MASTER_TIMEOUT_TICKS);
+}
+
+/* Overridden write function to support the rtc write */
+static esp_err_t i2c_master_write_to_device_custom(i2c_port_t i2c_num, uint8_t device_address, uint8_t reg_addr,
+                                     const uint8_t* write_buffer, size_t write_size,
+                                     TickType_t ticks_to_wait){
+    esp_err_t err = ESP_OK;
+
+    i2c_cmd_handle_t handle = i2c_cmd_link_create();
+    assert (handle != NULL);
+
+    err = i2c_master_start(handle);
+    if (err != ESP_OK) {
+        goto end;
+    }
+
+    err = i2c_master_write_byte(handle, device_address << 1 | I2C_MASTER_WRITE, true);
+    if (err != ESP_OK) {
+        goto end;
+    }
+
+    err = i2c_master_write_byte(handle, reg_addr, true);
+    if (err != ESP_OK) {
+        goto end;
+    }
+
+    err = i2c_master_write(handle, write_buffer, write_size, true);
+    if (err != ESP_OK) {
+        goto end;
+    }
+
+    i2c_master_stop(handle);
+    err = i2c_master_cmd_begin(i2c_num, handle, ticks_to_wait);
+
+end:
+    i2c_cmd_link_delete_static(handle);
+    return err;
 }
 
 /**
  * @brief Write a sequence of bytes to the registers
  */
 void rtc_register_write(uint8_t reg_addr, const uint8_t *data, size_t len){
-    i2c_master_write_to_device(I2C_MASTER_NUM, get_pcf85263_device_address(), data, len, I2C_MASTER_TIMEOUT_TICKS);
+    i2c_master_write_to_device_custom(I2C_MASTER_NUM, get_pcf85263_device_address(), reg_addr, data, len, I2C_MASTER_TIMEOUT_TICKS);
 
 }
 
